cb6ca488afa30031ea05ec0b4449aa3730705728
Kuiyou Hu <kuiyou.hu@sap.com>
2017-09-14 14:28:01 +0800

    initial setting

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="page-tabbar">
+    <div class="page-wrap">
+      <div class="page-title">书架</div>
+      <mt-tab-container class="page-tabbar-container" v-model="selected">
+        <mt-tab-container-item id="repo">
+          <!--<mt-cell v-for="n in 10" :title="'cell ' + n" />-->
+          <books></books>
+        </mt-tab-container-item>
+        <!--<mt-tab-container-item id="订单">
+          <mt-cell v-for="n in 5" :title="'订单 ' + n" />
+        </mt-tab-container-item>
+        <mt-tab-container-item id="发现">
+          <mt-cell v-for="n in 7" :title="'发现 ' + n" />
+        </mt-tab-container-item>-->
+        <mt-tab-container-item id="mine">
+          <div class="page-part">
+            <mt-cell v-for="n in 12" :title="'cell ' + n" />
+          </div>
+          <router-link to="/">
+            <mt-button type="danger" size="large">退出</mt-button>
+          </router-link>
+        </mt-tab-container-item>
+      </mt-tab-container>
+    </div>
+
+    <mt-tabbar v-model="selected" fixed>
+      <mt-tab-item id="repo">
+        <img slot="icon" src="./assets/100x100.png">
+        书架
+      </mt-tab-item>
+      <!--<mt-tab-item id="订单">
+        <img slot="icon" src="./assets/100x100.png">
+        订单
+      </mt-tab-item>
+      <mt-tab-item id="发现">
+        <img slot="icon" src="./assets/100x100.png">
+        发现
+      </mt-tab-item>-->
+      <mt-tab-item id="mine">
+        <img slot="icon" src="./assets/100x100.png">
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
+</template>
+
+<script>
+import Books from './components/Books';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      selected: 'repo',
+    };
+  },
+  components: { Books },
+};
+</script>
+
+<style>
+  .page-tabbar {
+    overflow: hidden;
+    height: 100vh;
+  }
+
+  .page-wrap {
+    overflow: auto;
+    height: 100%;
+    padding-bottom: 100px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import MintUI from 'mint-ui';
+import ElementUI from 'element-ui';
+import 'mint-ui/lib/style.css';
+import 'element-ui/lib/theme-default/index.css';
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+Vue.use(MintUI);
+Vue.use(ElementUI);
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App },
+});
